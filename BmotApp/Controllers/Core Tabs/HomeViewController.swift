d50be3fbb3f8469e95d50a5d2bc3af9b968e0bd0
jonathanAca06 <jagg0612996@gmail.com>
2021-11-05 17:52:26 -0600

    Proyecto :D

---
+++ BmotApp/Controllers/Core Tabs/HomeViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BmotApp
+//
+//  Created by jhon on 03/11/21.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
