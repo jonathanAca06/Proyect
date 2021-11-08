ceb8ce0cb33b0b5780b49821e21b495e802ca01d
jonathanAca06 <jagg0612996@gmail.com>
2021-11-08 13:13:40 -0600

    Modificaciones :D

--- BmotApp/Controllers/Core Tabs/GamesViewController.swift
+++ BmotApp/Controllers/Core Tabs/GamesViewController.swift
@@ -9,30 +9,62 @@ import UIKit
 
 class GamesViewController: UIViewController {
     
-    //MARK: Private Enunciado
+    //MARK: Variables privadas de uso
     //Data Source for CollectionView-1
-    private var items1 = ["felicidad", "afecto", "agradecimiento", "furia", "disgusto", "contento", "encanto", "soledad", "tristeza", "desconsuelo", "duda","enfado","timidez", "temor"]
+    private let emocion_color = [
+        "Miedo":"Verde oscuro",
+        "Afecto":"Verde claro",
+        "Tristeza":"Azul oscuro",
+        "Enojo":"Rojo",
+        "Alegria":"Amarillo"]
+    
+    private var dictyonary_items = [
+        "felicidad": "Alegria",
+        "afecto":"Afecto",
+        "agradecimiento":"Afecto",
+        "furia":"Enojo",
+        "disgusto":"Enojo",
+        "contento":"Alegria",
+        "encanto":"Alegria",
+        "soledad":"Tristeza",
+        "tristeza":"Tristeza",
+        "desconsuelo":"Tristeza",
+        "duda":"Miedo",
+        "enfado":"Enojo",
+        "timidez":"Miedo",
+        "temor":"Miedo"]
+    
+    private var items1 = [String]()
+    
+    //private var items1 = ["felicidad", "afecto", "agradecimiento", "furia", "disgusto", "contento", "encanto", "soledad", "tristeza", "desconsuelo", "duda","enfado","timidez", "temor"]
+    
+
     
     //Data Source for CollectionView-2
     private var items2 = [String]()
     
-    //Contador
+    //Variables
     private var cont: Int = 0
     private var timer = Timer()
+    private var pressbtn = false
     
     //MARK: Outlets
     @IBOutlet weak var collectionview_1: UICollectionView!
     
     @IBOutlet weak var collectionview_2: UICollectionView!
    
+    @IBOutlet weak var fondo_imageView: UIImageView!
     
 //MARK: View Lifecycle Methods
     override func viewDidLoad() {
         
         super.viewDidLoad()
         
+        //items1 configuracion de listado
+        self.items1 = Array(dictyonary_items.keys)
+        
         //CollectionView1 configuracion de Drag and Drop
-        self.collectionview_1.dragInteractionEnabled = true
+        self.collectionview_1.dragInteractionEnabled = false
         self.collectionview_1.dragDelegate = self
         self.collectionview_1.dropDelegate = self
         
@@ -44,18 +76,86 @@ class GamesViewController: UIViewController {
         
         
     }
-    
+    //MARK: Metodos de botones
+    //Metodo del boton play
     @IBAction func playButton(_ sender: UIButton) {
         timer.invalidate()
         
         timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(timer_action), userInfo: nil, repeats: true)
+        self.collectionview_1.dragInteractionEnabled = true
         
         
     }
     
-    
+    /// Metodo del boton stop
     @IBAction func stopButton(_ sender: UIButton) {
+        self.collectionview_1.dragInteractionEnabled = false
         timer.invalidate()
+        
+        var cont_emociones = [
+                    "Miedo":0,
+                    "Afecto":0,
+                    "Tristeza":0,
+                    "Enojo":0,
+                    "Alegria":0]
+        
+        for item in items2{
+            print("Contenido items2: \(item)")
+        }
+        
+        for code in items2{
+            if let mensaje = dictyonary_items[code]{
+                cont_emociones[mensaje]! += 1
+                
+            }else {
+                print("No hay rebanadas")
+            }
+        }
+         
+        
+        
+        for (emocion,valor) in cont_emociones{
+            print("Contenido cont_emociones: \(emocion):\(valor)")
+            if(emocion == "Miedo"  && valor > 0 && valor > cont_emociones["Afecto"]! && valor > cont_emociones["Tristeza"]! && valor > cont_emociones["Enojo"]! && valor > cont_emociones["Alegria"]!){
+                
+                //Cambio de color:
+                print("Cambio de color a: \(emocion_color[emocion]!)")
+                
+                self.fondo_imageView.backgroundColor = UIColor.init(red: 0, green: 123, blue: 0, alpha: 1)
+            }
+            if (emocion == "Afecto"  && valor > 0 && valor > cont_emociones["Miedo"]! && valor > cont_emociones["Tristeza"]! && valor > cont_emociones["Enojo"]! && valor > cont_emociones["Alegria"]!) {
+                
+                //Cambio de color:
+                print("Cambio de color a: \(emocion_color[emocion]!)")
+                
+                self.fondo_imageView.backgroundColor = UIColor.init(red: 0, green: 255, blue: 77, alpha: 0.9)
+            }
+            if (emocion == "Tristeza"  && valor > 0 && valor > cont_emociones["Miedo"]! && valor > cont_emociones["Afecto"]! && valor > cont_emociones["Enojo"]! && valor > cont_emociones["Alegria"]! ) {
+                
+                //Cambio de color:
+                print("Cambio de color a: \(emocion_color[emocion]!)")
+                self.fondo_imageView.backgroundColor = UIColor.init(red: 91, green: 0, blue: 255, alpha: 1)
+            }
+            if (emocion == "Enojo"  && valor > 0 && valor > cont_emociones["Miedo"]! && valor > cont_emociones["Afecto"]! && valor > cont_emociones["Tristeza"]! && valor > cont_emociones["Alegria"]!) {
+                
+                //Cambio de color:
+                print("Cambio de color a: \(emocion_color[emocion]!)")
+                
+                self.fondo_imageView.backgroundColor = UIColor.init(red: 255, green: 0, blue: 0, alpha: 1)
+            }
+            if (emocion == "Alegria"  && valor > 0 && valor > cont_emociones["Miedo"]! && valor > cont_emociones["Afecto"]! && valor > cont_emociones["Tristeza"]! && valor > cont_emociones["Enojo"]!) {
+                
+                //Cambio de color:
+                print("Cambio de color a: \(emocion_color[emocion]!)")
+               
+                self.fondo_imageView.backgroundColor = UIColor.init(red: 255, green: 255, blue: 0, alpha: 1)
+            }
+        
+        
+        }
+        //dump(cont_emociones)
+        
+        
         
     }
     
@@ -65,8 +165,25 @@ class GamesViewController: UIViewController {
         
     }
     
+    /// Metodo del boton reset
+    @IBAction func resetButton(_ sender: UIButton) {
+        print("Se reincio el juego")
+        self.pressbtn = true
+        if (self.pressbtn == true){
+            self.items2 = [String]()
+            self.cont = 0
+            self.fondo_imageView.backgroundColor = UIColor.white
+            self.collectionview_2.reloadData()
+            
+        }
+        self.collectionview_1.dragInteractionEnabled = false
+        
+        //print(self.cont)
+        //dump(items2)
+        
+    }
     
-    //MARK: Private Methods
+    //MARK: Metodos de funcionamiento
     
     /// This method moves a cell from source indexPath to destination indexPath within the same collection view. It works for only 1 item. If multiple items selected, no reordering happens.
     ///
@@ -260,3 +377,5 @@ extension GamesViewController : UICollectionViewDropDelegate
     }
     
 }
+
+
